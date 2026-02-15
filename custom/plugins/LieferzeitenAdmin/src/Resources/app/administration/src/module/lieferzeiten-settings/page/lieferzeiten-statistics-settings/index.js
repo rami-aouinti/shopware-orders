0219cdb81e8858d145fe7367058a4b93958db934
Rami <rami.aouinti@gmail.com>
2026-02-15 17:02:33 +0100

    Render statistics settings inside tabs page layout

---
+++ custom/plugins/LieferzeitenAdmin/src/Resources/app/administration/src/module/lieferzeiten-settings/page/lieferzeiten-statistics-settings/index.js
@@ -0,0 +1,6 @@
+import template from './lieferzeiten-statistics-settings.html.twig';
+import './lieferzeiten-statistics-settings.scss';
+
+Shopware.Component.register('lieferzeiten-statistics-settings', {
+    template,
+});
