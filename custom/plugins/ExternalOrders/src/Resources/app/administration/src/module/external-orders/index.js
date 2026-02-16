921b073beb47804f7ba471da47d2096875df2c20
Rami Aouinti <rami.aouinti@gmail.com>
2026-02-16 13:05:28 +0100

    tt

--- custom/plugins/ExternalOrders/src/Resources/app/administration/src/module/external-orders/index.js
+++ custom/plugins/ExternalOrders/src/Resources/app/administration/src/module/external-orders/index.js
@@ -22,9 +22,6 @@ Module.register('external-orders', {
         lieferzeit: {
             component: 'external-orders-lieferzeit-empty',
             path: 'lieferzeit',
-            redirect: {
-                name: 'lieferzeiten.index.lieferzeit',
-            },
             meta: {
                 privilege: 'admin',
             },
